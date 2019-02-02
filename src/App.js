6d774d6482df6ffac5da70f3189259cc3dec65a2
mbenjamin618 <mbenjamin618@gmail.com>
2019-02-02 12:21:47 -0500

    remove ;'s, move app into src folder, link react-native-gesture-handlers

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+})
 
 export default class App extends Component {
   render () {
@@ -25,7 +25,7 @@ export default class App extends Component {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -46,4 +46,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
