e8c40b4989717692ef2cb54717cf4c63299d41f6
mbenjamin618 <mbenjamin618@gmail.com>
2019-04-05 19:07:22 -0400

    revert to original reducer

--- src/components/Welcome/CardFeed/reducer.js
+++ src/components/Welcome/CardFeed/reducer.js
@@ -1,5 +1,4 @@
 import * as c from './constants'
-import produce from 'immer'
 
 const initialState = {
   fetchingImages: false,
@@ -11,24 +10,21 @@ const initialState = {
 }
 
 const actionHandlers = {
-  [c.GET_IMAGES_START]: (draft) => {
-      return draft.fetchingImages = true
+  [c.GET_IMAGES_START]: (state, action) => {
+    return {
+      ...state,
+      fetchingImages: true,
+    }
   },
-  [c.GET_IMAGES_SUCCESS]: (draft, action) => {
-    draft.fetchingImages = false
-    draft.imageObjects = action.imageObject.entities.images
-    draft.imageIds = action.imageObject.result
-    draft.page = action.page
-    draft.imageError = null
-    return draft
-    // return {
-    //   ...state,
-    //   fetchingImages: false,
-    //   imageObjects: { ...state.imageObjects, ...action.imageObject.entities.images },
-    //   imageIds: [...state.imageIds, ...action.imageObject.result],
-    //   page: action.page,
-    //   imageError: null,
-    // }
+  [c.GET_IMAGES_SUCCESS]: (state, action) => {
+    return {
+      ...state,
+      fetchingImages: false,
+      imageObjects: { ...state.imageObjects, ...action.imageObject.entities.images },
+      imageIds: [...state.imageIds, ...action.imageObject.result],
+      page: action.page,
+      imageError: null,
+    }
   },
   [c.GET_IMAGES_ERROR]: (state, action) => {
     return {
